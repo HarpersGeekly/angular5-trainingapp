df5d80b8eb7928cd0901004e95c551805fa72122
harpersgeekly <harperryanc@gmail.com>
2019-01-31 13:06:15 -0600

    routes module in place for paths

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PostsIndexComponent } from './posts-index/posts-index.component';
+
+const routes: Routes = [
+  { path: '', component: PostsIndexComponent}
+];
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ], // You first must initialize the router and start it listening for browser location changes.
+  // The method is called forRoot() because you configure the router at the application's root level.
+  // The forRoot() method supplies the service providers and directives needed for routing,
+  // and performs the initial navigation based on the current browser URL.
+
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { PostsIndexComponent } from './posts-index/posts-index.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { PostService} from './services/post.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsIndexComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
