55abf2ab32bea101d04187f215623072c48555d9
harpersgeekly <harperryanc@gmail.com>
2019-02-05 11:20:48 -0600

    added a users component soI have quick access to a list of users

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from '../services/user.service';
+import {User} from '../models/user';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  users: User[];
+  constructor(private userSvc: UserService) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.userSvc.getUsers()
+      .subscribe(users => {
+        this.users = users;
+        console.log(users); // .subscribe() is like .then(), response is users that I assign to the variable users
+      });
+  }
+
+}
