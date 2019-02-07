4d30608141c8100f1c7ea4fdb1a2afcc51d93096
harpersgeekly <harperryanc@gmail.com>
2019-02-07 15:57:39 -0600

    login component manages the login form, validations, loading spinner, and userService calls logic. When a user comes over that has a token, that user is stored in localStorage for the time being. A page reload will wipe them out for example. Need newer approach but works for now.

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import {AlertService} from '../services/alert.service';
+import {UserService} from '../services/user.service';
+import {User} from '../models/user';
+import {toPromise} from 'rxjs/operator/toPromise';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: 'login.component.html'
+})
+
+export class LoginComponent implements OnInit {
+
+  // The login component uses the authentication service to login and logout of the application.
+  // It automatically logs the user out when it initializes (ngOnInit) so the login page can also be used to logout.
+
+  model: any = {};
+  loading = false;
+  errorLoggingIn = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService) {
+  }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.errorLoggingIn = false;
+    this.loading = true;
+    this.userService.login(this.model).toPromise().then(response => {
+      if (response && response.token) {
+        localStorage.setItem('currentUser', JSON.stringify(response));
+        this.userService.loggedInUser = response;
+        this.router.navigate(['/user/profile/' + this.userService.loggedInUser.id + '/' + this.userService.loggedInUser.username]);
+      } else {
+          this.errorLoggingIn = true;
+          this.loading = false;
+        }
+    });
+  }
+}
+
