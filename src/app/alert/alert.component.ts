aa644ce171ceffc286aa88a99c4f4bc16b819727
harpersgeekly <harperryanc@gmail.com>
2019-02-08 10:46:35 -0600

    alert service that you can make custom alerts. alerts can be redirected. alerts styling can be changed and close after clicking on them

---
+++ src/app/alert/alert.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {AlertService} from '../services/alert.service';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.css']
+})
+export class AlertComponent implements OnInit {
+
+  // The alert component passes alert messages to the template whenever a message is received from the alert service.
+  // It does this by subscribing to the alert service's getMessage() method which returns an Observable.
+  message: any;
+
+  constructor(private alertService: AlertService) { }
+
+  ngOnInit() {
+    this.alertService.getMessage().subscribe(message => { this.message = message; });
+  }
+
+  close() {
+    this.message = !this.message;
+  }
+
+}
