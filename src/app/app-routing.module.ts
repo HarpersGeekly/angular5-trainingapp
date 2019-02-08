27e8fdffa26c4b25227501467ebe5bd95c527917
harpersgeekly <harperryanc@gmail.com>
2019-02-08 16:04:54 -0600

    added a lot more imports for various services and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostsIndexComponent } from './posts-index/posts-index.component';
 import { PostShowComponent } from './post-show/post-show.component';
 import {UserProfileComponent} from './user-profile/user-profile.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {AuthGuard} from './services/authGuard.service';
+import {EditUserComponent} from './edit-user/edit-user.component';
 
 const routes: Routes = [
   { path: '', component: PostsIndexComponent},
   { path: 'post/:id/:title', component: PostShowComponent},
-  { path: 'user/profile/:id/:username', component: UserProfileComponent}
+  { path: 'user/profile/:id/:username', component: UserProfileComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'register/:success', component: EditUserComponent},
+  { path: 'login', component: LoginComponent}
 ];
-
+/*canActivate: [AuthGuard]*/
 @NgModule({
   exports: [ RouterModule ],
   imports: [ RouterModule.forRoot(routes) ], // You first must initialize the router and start it listening for browser location changes.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PostsIndexComponent } from './posts-index/posts-index.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { PostService} from './services/post.service';
 import { PostShowComponent } from './post-show/post-show.component';
@@ -11,7 +11,17 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RegisterComponent } from './register/register.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserService } from './services/user.service';
-import {NgbAlertConfig, NgbDropdownConfig, NgbModule, NgbTabsetConfig} from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbAlertConfig, NgbDropdownConfig, NgbModule, NgbTabsetConfig} from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './services/authGuard.service';
+import { AlertService} from './services/alert.service';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { UsersComponent } from './users/users.component';
+import { EmailValidatorDirective } from './directives/email-validator.directive';
+import { UsernameValidatorDirective } from './directives/username-validator.directive';
+import { AlertComponent } from './alert/alert.component';
+// import { JwtInterceptor } from './services/jwt-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -21,14 +31,31 @@ import {NgbAlertConfig, NgbDropdownConfig, NgbModule, NgbTabsetConfig} from '@ng
     NavbarComponent,
     RegisterComponent,
     UserProfileComponent,
+    LoginComponent,
+    EditUserComponent,
+    UsersComponent,
+    EmailValidatorDirective,
+    UsernameValidatorDirective,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    NgbModule
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
-  providers: [PostService, UserService, NgbAlertConfig, NgbDropdownConfig, NgbTabsetConfig],
+  providers: [PostService, UserService,
+    // {
+    // provide: HTTP_INTERCEPTORS,
+    // useClass: JwtInterceptor,
+    // multi: true
+  // },
+  AuthGuard,
+    AlertService, EmailValidatorDirective, UsernameValidatorDirective,
+    // JwtInterceptor,
+    NgbAlertConfig, NgbDropdownConfig, NgbTabsetConfig],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
