6c11fa92b5eacc5a94af3729fa191d26fb6f02aa
harpersgeekly <harperryanc@gmail.com>
2019-02-13 13:28:07 -0600

    changed route for editing to post/edit/id/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostsIndexComponent } from './posts-index/posts-index.component';
 import { PostShowComponent } from './post-show/post-show.component';
 import {UserProfileComponent} from './user-profile/user-profile.component';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './user-login/login.component';
+import {RegisterComponent} from './user-register/register.component';
 import {AuthGuard} from './services/authGuard.service';
-import {EditUserComponent} from './edit-user/edit-user.component';
+import {UserEditComponent} from './user-edit/user-edit-component';
 import {PostCreateComponent} from './post-create/post-create.component';
+import {PostEditComponent} from './post-edit/post-edit.component';
 
 const routes: Routes = [
   { path: '', component: PostsIndexComponent},
   { path: 'post/:id/:title', component: PostShowComponent},
-  { path: 'post/create', component: PostCreateComponent, canActivate: [AuthGuard]},
+  { path: 'post/create', component: PostCreateComponent, },
+  { path: 'post/edit/id/:id', component: PostEditComponent},
   { path: 'user/profile/:id/:username', component: UserProfileComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'register/:success', component: EditUserComponent},
+  { path: 'register/:success', component: UserEditComponent},
   { path: 'login', component: LoginComponent}
 ];
-
+// , canActivate: [AuthGuard]}
 @NgModule({
   exports: [ RouterModule ],
   imports: [ RouterModule.forRoot(routes) ], // You first must initialize the router and start it listening for browser location changes.
