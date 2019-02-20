5ce205626ab30e7fd25b32103ccb6fbd75ddd9ee
harpersgeekly <harperryanc@gmail.com>
2019-02-19 18:27:24 -0600

    make a call to the backend for voting and removing votes. takes in a postid, userid, and type or vote, -1 or 1. removeVote doesn't need a type.
    getpost() slightly changed.
    If you're logged in, your id gets added to the query param to be used on the backend to check if you've voted on the post yet. If you're not logged in, getPost() runs like normal.

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Post} from '../models/post';
+import {PostVote} from '../models/postVote';
+import {UserService} from './user.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -11,8 +13,10 @@ const httpOptions = {
 export class PostService {
   private postsUrl = '/api/post';  // URL to rest api, look at file: proxy.conf.json
   successfulDelete: boolean;
+  vote: PostVote;
+  userId: number;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private userSvc: UserService) { }
 
   getPosts(id: number): Observable<Post[]> {
     let url: string = this.postsUrl;
@@ -25,7 +29,12 @@ export class PostService {
   }
 
   getPost(id: number): Observable<Post> {
-    return this.http.get<Post>(this.postsUrl + '/postById/' + id);
+    if (this.userSvc.loggedInUser != null) {
+      this.userId = this.userSvc.loggedInUser.id;
+      return this.http.get<Post>(this.postsUrl + '/postById/' + id + '?userId=' + this.userId.toString());
+    } else {
+      return this.http.get<Post>(this.postsUrl + '/postById/' + id);
+    }
   }
 
   createPost(post: Post) {
@@ -39,19 +48,36 @@ export class PostService {
   deletePost(id: number) {
     this.successfulDelete = true;
     return this.http.delete(this.postsUrl + '/deletePost/' + id);
-    //   {headers: httpOptions.headers, observe: 'response'}).subscribe(response => {
-    //     console.log(response.status);
-    //     if (response.status === 200) {
-    //       console.log('back from deleting in REST API');
-    //       this.successfulDelete = true;
-    //     } else {
-    //       console.log('else false');
-    //       this.successfulDelete = false;
-    //     }
-    //     console.log(this.successfulDelete);
-    //     return this.successfulDelete;
-    //   },
-    //   // error goes here );
-    // );
+  }
+
+  postVote(postId: number, userId: number, type: number): Observable<Post> {
+    return this.http.post<Post>(this.postsUrl + '/postVote/' + postId + '/' + userId + '/' + type, this.vote);
+  }
+
+  removeVote(postId: number, userId: number): Observable<Post> {
+    return this.http.post<Post>(this.postsUrl + '/removeVote/' + postId + '/' + userId, this.vote);
   }
 }
+
+
+
+
+
+
+
+
+
+//   {headers: httpOptions.headers, observe: 'response'}).subscribe(response => {
+//     console.log(response.status);
+//     if (response.status === 200) {
+//       console.log('back from deleting in REST API');
+//       this.successfulDelete = true;
+//     } else {
+//       console.log('else false');
+//       this.successfulDelete = false;
+//     }
+//     console.log(this.successfulDelete);
+//     return this.successfulDelete;
+//   },
+//   // error goes here );
+// );
